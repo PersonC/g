00a517e2e71d119b3fb146d34455dc14ebd99fbd
nikolay.lumpov <nikolay.lumpov@H2O>
2020-09-06 21:26:39 +0300

    third

--- NameMethodsClass/src/a1.java
+++ NameMethodsClass/src/a1.java
@@ -1,32 +1,32 @@
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class a1 {
 
 	public static void main(String[] args) {
-		for(Method method: Object.class.getMethods()) {
-			System.out.println(method);
-		}
-		System.out.println("==Methods=====");
-		for(Method method: Class.class.getMethods()) {
-			System.out.println(method);
-		}
-		System.out.println("==Declared Methods=====");
-		for(Method method: Class.class.getDeclaredMethods()) {
-			System.out.println(method);
-		}
+
 		System.out.println("==Constructor=====");
-		for(Constructor cons: Calendar.class.getDeclaredConstructors()) {
+		for(Constructor cons: ArrayList.class.getDeclaredConstructors()) {
 			System.out.println(cons);
 		}
+		System.out.println("==Declared Methods=====");
+		for(Method method: ArrayList.class.getDeclaredMethods()) {
+			System.out.println(method);
+		}
+		
+		System.out.println("==Methods=====");
+		for(Method method: ArrayList.class.getMethods()) {
+			System.out.println(method);
+		}
 		System.out.println("===fields====");
-		for(Field f: Calendar.class.getFields()) {
+		for(Field f: ArrayList.class.getFields()) {
 			System.out.println(f);
 		}
 		System.out.println("=== declared fields====");
-		for(Field f: Calendar.class.getDeclaredFields()) {
+		for(Field f: ArrayList.class.getDeclaredFields()) {
 			System.out.println(f);
 		}
 	}
--- pnl/src/pnl/pnl.java
+++ pnl/src/pnl/pnl.java
@@ -1,10 +1,12 @@
 package pnl;
 
+import java.util.ArrayList;
+
 public class pnl {
 
 	public static void main(String[] args) {
 		int n = 20;
-		X x = new X(n);
+		X x = new X(n,"x1");
 		for(int i=0;i<n;i++) {
 			x.x[i] = (double)i+1;
 		}
@@ -18,12 +20,18 @@ public class pnl {
 		x2.calc();
 		System.out.println(x2.toPrint());
 		
-		X y = new X(n);
+		X y = new X(n,"y");
 		for(int i=0;i<n;i++) {
 			y.x[i] = 1.0 + 2.0 * x.x[i] + 3.0 * x2.x[i];
 		}
 		y.calc();
 		System.out.println(y.toPrint());
+		
+		Sampling model = new Sampling(2,0);
+		model.x.add(x);
+		model.x.add(x2);
+		model.y.add(y);
+		
 		
 
 	}
